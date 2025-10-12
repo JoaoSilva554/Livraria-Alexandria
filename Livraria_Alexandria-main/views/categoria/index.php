aaec200b47a969dfaf04b70a79dbf9df9f516a25
JoaoSilva554 <capitaolevi53@gmail.com>
2025-10-12 17:48:01 -0300

    Update index.php

--- Livraria_Alexandria-main/views/categoria/index.php
+++ Livraria_Alexandria-main/views/categoria/index.php
@@ -6,11 +6,41 @@
         <div class="card-body">
             <label for="email">Digite seu e-mail:</label>
             <input type="email" name="email" id="email" required
-            class="form-control">
+            class="form-control"
+            data-parsley-required-message="Campo obrigatório"
+            data-parsley-type-message="E-mail inválido">
+        
+        
 
             <label for="senha">Digite sua senha:</label>
             <div class="input-group mb-3">
                 <input type="password" name="senha" id="senha" required
-               class="form-control">
+               class="form-control"
+               data-parsley-required-message="Senha obrigatória"
+               data-parsley-errors-container="#erroSenha">
+               
+               //fazer botão de exibir senha
+               
+               <span id ="error"></span>
+
+               //fazer botão de login
+
+
+
+            
+
+
+
                
 </form>
+<script>
+    function mostrarSenha() {
+        var campo = document.getElementById("senha");
+        if (campo.type == "password") {
+            campo.type = "text";
+        } else {
+            campo.type = "password";
+        }
+
+    }
+</script>    
